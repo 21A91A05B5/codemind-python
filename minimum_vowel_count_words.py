42061119ddb2dc55f2990d3b47fa152262e05355
21A91A05B5 <97424445+21A91A05B5@users.noreply.github.com>
2022-07-17 10:06:42 +0530

    s=input() z=s.split() l=[] for i in z:     vowel=sum(letter in 'aeiou' for letter in i.lower())     l.append(vowel) print(l.count(min(l)))

--- minimum_vowel_count_words.py
+++ minimum_vowel_count_words.py
@@ -2,6 +2,6 @@ s=input()
 z=s.split()
 l=[]
 for i in z:
-    vowel=sum(letter in'aeiou' for letter in i.lower())
+    vowel=sum(letter in 'aeiou' for letter in i.lower())
     l.append(vowel)
 print(l.count(min(l)))
