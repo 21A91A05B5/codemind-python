8fa2091ee1728ad6fc69e73c2f0d40be00593bbe
21A91A05B5 <97424445+21A91A05B5@users.noreply.github.com>
2022-07-21 20:16:04 +0530

    s=input() v="aeiouAEIOU" l=[] c=0 for i in s:     if i in v and i not in l:         l.append(i) print(*l)

--- vowels_in__string.py
+++ vowels_in__string.py
@@ -1,9 +1,8 @@
 s=input()
-v='aeiouAEIOU'
+v="aeiouAEIOU"
+l=[]
 c=0
-for i in sorted(set(s),key=s.index):
-    if i in v:
-        c+=1
-        print(i,end=' ')
-if(c==0):
-    print('-1')
+for i in s:
+    if i in v and i not in l:
+        l.append(i)
+print(*l)
