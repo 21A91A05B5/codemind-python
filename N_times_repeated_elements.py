5d2cf116ea32a3bc10ed79209a612a8ee16dc60a
21A91A05B5 <97424445+21A91A05B5@users.noreply.github.com>
2022-08-30 22:55:51 +0530

    n=int(input()) l=list(map(int,input().split( ))) k=int(input()) s=[] c=0 for i in l:     if i not in s:         s.append(i) for i in s:     if(l.count(i)==k):         c+=1         print(i,end=' ') if(c==0):     print("-1")

---
+++ N_times_repeated_elements.py
@@ -0,0 +1,15 @@
+n=int(input())
+l=list(map(int,input().split( )))
+k=int(input())
+s=[]
+c=0
+for i in l:
+    if i not in s:
+        s.append(i)
+for i in s:
+    if(l.count(i)==k):
+        c+=1
+        print(i,end=' ')
+if(c==0):
+    print("-1")
+        
