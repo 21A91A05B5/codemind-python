3b66e356625a9461834840d685756d91f2333986
chanda.Sri Durga <97424445+21A91A05B5@users.noreply.github.com>
2022-10-03 19:33:50 +0530

    n=list(map(str,input().split())) c=0 d=[] for i in n:     for j in i:         if j=='a' or j=='e' or j=='i' or j=='o' or j=='u' or j=='A' or j=='E' or j=='I' or j=='O' or j=='U':             c+=1     d.append(c)     c=0 m=max(d) print(m)

--- maximum_vowel_count.py
+++ maximum_vowel_count.py
@@ -1,7 +1,11 @@
-s=input()
-z=s.split()
-l=[]
-for i in z:
-    vowel=sum(letter in 'aeiou' for letter in i.lower())
-    l.append(vowel)
-print(max(l))
+n=list(map(str,input().split()))
+c=0
+d=[]
+for i in n:
+    for j in i:
+        if j=='a' or j=='e' or j=='i' or j=='o' or j=='u' or j=='A' or j=='E' or j=='I' or j=='O' or j=='U':
+            c+=1
+    d.append(c)
+    c=0
+m=max(d)
+print(m)
