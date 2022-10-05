aecac69e5c03ef2fae2d5fa8f3b7f1ac01fbd43a
chanda.Sri Durga <97424445+21A91A05B5@users.noreply.github.com>
2022-10-05 14:43:07 +0530

    s=input() c=[] d=[] for i in s:     if i=='a' or i=='e' or i=='i' or i=='o' or i=='u':         c.append(i)     elif i=='A' or i=='E' or i=='I' or i=='O' or i=='U':         d.append(i) k=len(set(c)) l=len(set(d)) if(k==5 or l==5):     print("True") else:     print("False")

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,16 +1,14 @@
-n=input()
+s=input()
 c=[]
 d=[]
-for i in n:
+for i in s:
     if i=='a' or i=='e' or i=='i' or i=='o' or i=='u':
         c.append(i)
-    elif  i=='A' or i=='E' or i=='I' or i=='O' or i=='U':
+    elif i=='A' or i=='E' or i=='I' or i=='O' or i=='U':
         d.append(i)
-a=len(set(c))
-b=len(set(d))
-if a==5:
-    print(True)
-elif b==5:
-    print(True)
+k=len(set(c))
+l=len(set(d))
+if(k==5 or l==5):
+    print("True")
 else:
-    print(False)
+    print("False")
