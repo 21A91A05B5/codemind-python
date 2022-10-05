d40d2a276df4a740ebf47a52ee0bbe72507ed7e5
chanda.Sri Durga <97424445+21A91A05B5@users.noreply.github.com>
2022-10-05 21:19:53 +0530

    n=int(input()) l=list(map(int,input().split( ))) c=[] d=[] for i in range(n):     if(l[i]%2==0):         c.append(l[i])     else:         d.append(l[i]) i=0 j=0 while(i<len(c) or j<len(d)):     if(i<len(c)):         print(c[i],end=" ")         i+=1     if(j<len(d)):         print(d[j],end=" ")         j+=1 if(n%2!=0):     print("0")

--- even_odd_pairs.py
+++ even_odd_pairs.py
@@ -1,20 +1,20 @@
 n=int(input())
-a=list(map(int,input().split( )))
-l=[]
-k=[]
+l=list(map(int,input().split( )))
+c=[]
+d=[]
 for i in range(n):
-    if(a[i]%2==0):
-        l.append(a[i])
+    if(l[i]%2==0):
+        c.append(l[i])
     else:
-        k.append(a[i])
+        d.append(l[i])
 i=0
 j=0
-while(i<len(l) or j<len(k)):
-    if(i<len(l)):
-        print(l[i],end=" ")
+while(i<len(c) or j<len(d)):
+    if(i<len(c)):
+        print(c[i],end=" ")
         i+=1
-    if(j<len(k)):
-        print(k[j],end=" ")
+    if(j<len(d)):
+        print(d[j],end=" ")
         j+=1
 if(n%2!=0):
     print("0")
